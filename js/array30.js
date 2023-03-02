be00a8b6e68a910ec4b48813ca667c6cceb4149e
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-02 08:03:23 +0100

    array 30

---
+++ js/array30.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    
+    var result = "";
+    
+    for (var i = n - 1; i > -1; i--)
+        {
+            result = result + arr[i] + " ";
+        }
+    console.log(result);
+}
