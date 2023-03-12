f534a55969920363bc7359793f509cf9b539950f
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-12 15:15:12 +0200

    shift the linked list

--- js/LinkedList/SinglyLL.js
+++ js/LinkedList/SinglyLL.js
@@ -47,7 +47,16 @@ class SinglyLinkedList{
         }
         return current;
     }
-    
+    shift(){
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length ===0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
 }
 
 // var first = new Node("Hi");
