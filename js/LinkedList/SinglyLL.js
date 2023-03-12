e8c1089fa78bc7226bbb9b3261d0a67f9f5d92ba
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-12 10:52:00 +0200

    pop value

--- js/LinkedList/SinglyLL.js
+++ js/LinkedList/SinglyLL.js
@@ -30,6 +30,23 @@ class SinglyLinkedList{
             current = current.next;
         }
     }
+    pop(){
+        if(!this.head) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length ===0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
     
 }
 
