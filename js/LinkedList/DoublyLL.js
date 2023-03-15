5311500324a84d0ee1add6a37a2b2b5ce97d618e
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-15 15:17:16 +0200

    unshift DLL

--- js/LinkedList/DoublyLL.js
+++ js/LinkedList/DoublyLL.js
@@ -50,4 +50,17 @@ class DoublyLinkedList{
         this.head.prev = null;
         oldHead.next = null;
     }
+    unshift(val){
+        var newNode = new Node(val);
+        if(this.length ===0){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
