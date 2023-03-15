470d034d5861a48a2cf7129c0a22a9b5d1389e8f
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-15 15:53:51 +0200

    set the val dll

--- js/LinkedList/DoublyLL.js
+++ js/LinkedList/DoublyLL.js
@@ -84,4 +84,12 @@ class DoublyLinkedList{
         }
         
     }
+    set(index,val){
+        var foundNode = this.get(index);
+        if(foundNode != null){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 }
