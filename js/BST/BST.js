e1bd6a198da98fee924f43f2ae8b6001c795a29a
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-20 15:03:47 +0200

    find the value of BST

--- js/BST/BST.js
+++ js/BST/BST.js
@@ -37,4 +37,18 @@ class BinarySearchTree{
             }
         }
     }
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root, found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            }else if(value > current.value){
+                current = current.right;
+            }else{
+                found = true;
+            }
+        }
+        return current;
+    }
 }
