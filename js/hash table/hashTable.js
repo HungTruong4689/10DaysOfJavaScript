eb80c0fc228709b6f120f3a02da20a1741d54533
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-28 14:25:02 +0300

    set value hash map

--- js/hash table/hashTable.js
+++ js/hash table/hashTable.js
@@ -13,7 +13,16 @@ class HashTable {
       }
       return total;
     }
-    
+    set(key,value){
+      let index = this._hash(key);
+      if(!this.keyMap[index]){
+        this.keyMap[index] = [];
+      }
+      this.keyMap[index].push([key,value]);
+    }
+    get(key){
+
+    }
   }
   
   let ht = new HashTable(17);
