fd172b897fe336b2eb1a6222a724a74dd75ffeda
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-04 21:47:02 +0300

    new test react app

--- js/graph/test.js
+++ js/graph/test.js
@@ -1,16 +1,27 @@
 import * as React from 'react';
 
-class MyComponent{
-    handleMouseOver(){
-        const { iconWhite} = this.props.content;
-        this.setState({
-            img: iconWhite,
-        })
-    }
-    handleMouseOut(){
-        const { iconStandard} = this.props.content;
-        this.setState({
-            img: iconStandard,
-        })
-    }
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    const { iconStandard } = this.props.content;
+    this.state = {
+      img: iconStandard,
+    };
+    this.handleMouseAction = this.handleMouseAction.bind(this);
+  }
+
+  handleMouseAction(icon) {
+    this.setState({
+      img: icon,
+    });
+  }
+
+  render() {
+    const { iconWhite, iconStandard } = this.props.content;
+    return (
+      <div onMouseOver={() => this.handleMouseAction(iconWhite)} onMouseOut={() => this.handleMouseAction(iconStandard)}>
+        <img src={this.state.img} alt="Icon" />
+      </div>
+    );
+  }
 }
