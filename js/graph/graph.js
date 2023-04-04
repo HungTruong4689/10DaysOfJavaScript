ab3403dbde982ad5aa116f606ea3776b3dc69d3d
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-04 15:59:10 +0300

    check graph addVertex

--- js/graph/graph.js
+++ js/graph/graph.js
@@ -2,9 +2,13 @@ class Graph{
     constructor(){
         this.adjecencyList = {};
     }
+    addVertex(vertex){
+        if(!this.adjecencyList[vertex] ) this.adjecencyList[vertex] = [];
+    }
 }
 
 let g = new Graph();
 g.addVertex("Dallas");
 g.addVertex("Tokyo");
 g.addVertex("Aspen");
+console.log(g)
