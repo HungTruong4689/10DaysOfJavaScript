da57ec21a4f52207bbfe10d7fcb467f842acf28f
HungTruong4689 <hungphuc4689@gmail.com>
2023-12-05 10:48:12 +0200

    statement

---
+++ refactoring/statment.js
@@ -0,0 +1,12 @@
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+  for (let perl of invoice.performance) {
+  }
+}
